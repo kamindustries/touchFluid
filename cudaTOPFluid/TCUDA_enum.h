42ecb5aa303553a76f924f0b17b27a14344d982f
Kurt Kaminski <kurtoon@gmail.com>
2016-02-27 19:43:45 -0800

    Adding TCUDA_enum.h and .cpp to print strings from enumerated types

---
+++ cudaTOPFluid/TCUDA_enum.h
@@ -0,0 +1,17 @@
+#ifndef __TCUDA_ENUM_Types__
+#define __TCUDA_ENUM_Types__
+
+extern char* TCUDA_DataType_enum[];
+extern char* TCUDA_ProjectionType_enum[];
+extern char* TCUDA_ObjSubType_enum[];
+extern char* TCUDA_DataFormat_enum[];
+extern char* TCUDA_ParamDataFormat_enum[];
+extern char* TCUDA_DataLocation_enum[];
+extern char* TCUDA_ChanOrder_enum[];
+extern char* TCUDA_OutputType_enum[];
+extern char* TCUDA_PixelFormat_enum[];
+extern char* TCUDA_ObjParamType_enum[];
+extern char* TCUDA_FogType_enum[];
+extern char* TCUDA_MemType_enum[];
+
+#endif
