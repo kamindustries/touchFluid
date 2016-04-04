3c225edb23340b550f1f88240fe4b383a5f1cbdf
Kurt Kaminski <kurtoon@gmail.com>
2016-04-04 15:26:51 -0700

    moving some functions to util_functions.hpp, adding util_functions.hpp, .gitignore a test folder (cudaTOPFluid2)

---
+++ cudaTOPFluid/util_functions.hpp
@@ -0,0 +1,49 @@
+#ifndef __CUDA_TOP_FLUID_UTIL_FUNCTIONS__
+#define __CUDA_TOP_FLUID_UTIL_FUNCTIONS__
+
+#include <string>
+
+bool hasEnding (std::string const &fullString, std::string const &ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+bool hasBeginning (std::string const &fullString, std::string const &beginning) {
+	if (fullString.find(beginning) != std::string::npos )
+		return true;
+	else 
+		return false;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Print connected node information
+///////////////////////////////////////////////////////////////////////////////
+void printNodeInfo(const int nparams, const TCUDA_ParamInfo **params){
+	printf("\n----------\nINCOMING PARAMETERS:\n");
+	printf("%d nodes connected\n\n", nparams);
+
+	for (int i = 0; i < nparams; i++) {
+		printf("Node %d: %s\n", params[i]->inputNumber, params[i]->name);
+		printf("%d values\n", params[i]->param.numValues);
+		if (params[i]->dataType == TCUDA_DATA_TYPE_TOP){
+			printf("  TOP INFO:\n");
+			printf("  w: %d, h: %d\n", params[i]->top.width, params[i]->top.height);
+			printf("  %s\n", TCUDA_ChanOrder_enum[params[i]->top.chanOrder]); 
+		}
+		if (params[i]->dataType == TCUDA_DATA_TYPE_CHOP){
+			printf("  CHOP INFO:\n");
+			printf("  Num channels: %d\n", params[i]->chop.numChannels); 
+			printf("  Length: %d\n", params[i]->chop.length);
+			printf("  Sample rate: %f\n", params[i]->chop.sampleRate); 
+		}
+		printf("\n");
+	}
+	printf("----------\n\n");
+}
+
+
+#endif
